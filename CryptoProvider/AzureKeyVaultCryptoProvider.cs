e28eb2c9714091ef43334ffb8f6c0c29d9d0db4e
Vitaliy Slepakov <v.slepakov@gmail.com>
2021-10-25 15:20:24 +0200

    refactoring identity service provider

--- CryptoProvider/AzureKeyVaultCryptoProvider.cs
+++ CryptoProvider/AzureKeyVaultCryptoProvider.cs
@@ -12,17 +12,6 @@
     {
         private const int RSA_OAEP_PADDING_SIZE_IN_BYTES = 42;
 
-        public Task<string> DecryptAsync(string ciphertext, KeyOptions keyOptions, CancellationToken ct = default)
-        {
-            return keyOptions.KeyType switch
-            {
-                KeyType.RSA => RsaDecryptAsync(ciphertext, keyOptions, ct),
-                KeyType.ECC => throw new NotImplementedException(),
-                KeyType.Symmetric => throw new NotImplementedException(),
-                _ => throw new ArgumentException($"{keyOptions.KeyType} is not supported by this provider"),
-            };
-        }
-
         public Task<string> EncryptAsync(string plaintext, KeyOptions keyOptions, CancellationToken ct = default)
         {
             return keyOptions.KeyType switch
@@ -33,20 +22,15 @@
                 _ => throw new ArgumentException($"{keyOptions.KeyType} is not supported by this provider"),
             };
         }
-
-        private async Task<string> RsaDecryptAsync(string ciphertext, KeyOptions keyOptions, CancellationToken ct = default)
+        public Task<string> DecryptAsync(string ciphertext, KeyOptions keyOptions, CancellationToken ct = default)
         {
-            var ciphertextBytes = Convert.FromBase64String(ciphertext);
-
-            if (ciphertextBytes.Length > keyOptions.KeySize / 8)
+            return keyOptions.KeyType switch
             {
-                throw new DataTooLargeException($"Data too large to decrypt using RSA-OAEP with the key size {keyOptions.KeySize}");
-            }
-
-            var cryptographyClient = new CryptographyClient(new Uri(keyOptions.KeyId), new EnvironmentCredential());
-            var decryptResult = await cryptographyClient.DecryptAsync(EncryptionAlgorithm.RsaOaep, ciphertextBytes, ct);
-
-            return Encoding.UTF8.GetString(decryptResult.Plaintext);
+                KeyType.RSA => RsaDecryptAsync(ciphertext, keyOptions, ct),
+                KeyType.ECC => throw new NotImplementedException(),
+                KeyType.Symmetric => throw new NotImplementedException(),
+                _ => throw new ArgumentException($"{keyOptions.KeyType} is not supported by this provider"),
+            };
         }
 
         private async Task<string> RsaEncryptAsync(string plaintext, KeyOptions keyOptions, CancellationToken ct = default)
@@ -62,6 +46,21 @@
             var encryptResult = await cryptographyClient.EncryptAsync(EncryptionAlgorithm.RsaOaep, plaintextBytes, ct);
 
             return Convert.ToBase64String(encryptResult.Ciphertext);
+        }
+
+        private async Task<string> RsaDecryptAsync(string ciphertext, KeyOptions keyOptions, CancellationToken ct = default)
+        {
+            var ciphertextBytes = Convert.FromBase64String(ciphertext);
+
+            if (ciphertextBytes.Length > keyOptions.KeySize / 8)
+            {
+                throw new DataTooLargeException($"Data too large to decrypt using RSA-OAEP with the key size {keyOptions.KeySize}");
+            }
+
+            var cryptographyClient = new CryptographyClient(new Uri(keyOptions.KeyId), new EnvironmentCredential());
+            var decryptResult = await cryptographyClient.DecryptAsync(EncryptionAlgorithm.RsaOaep, ciphertextBytes, ct);
+
+            return Encoding.UTF8.GetString(decryptResult.Plaintext);
         }
     }
 }
--- CryptoProvider/IdentityServiceCryptoProvider.cs
+++ CryptoProvider/IdentityServiceCryptoProvider.cs
@@ -1,5 +1,6 @@
 ﻿namespace EdgeSecrets.CryptoProvider
 {
+    using System;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading;
@@ -12,6 +13,9 @@
         private const string ENCRYPT_ENDPOINT = "http://keyd.sock/encrypt?api-version=2021-05-01";
         private const string DECRYPT_ENDPOINT = "http://keyd.sock/decrypt?api-version=2021-05-01";
         private const string KEYD_SOCKET = "/run/aziot/keyd.sock";
+        private const string SYMMETRIC_ALGORITHM = "AEAD";
+        private const string ASYMMETRIC_ALGORITHM = "RSA-PKCS1";
+
         private readonly HttpClient _httpClient;
 
         public IdentityServiceCryptoProvider()
@@ -19,45 +23,57 @@
             _httpClient = Util.HttpClientHelper.GetUnixDomainSocketHttpClient(KEYD_SOCKET, CancellationToken.None);
         }
 
-        public async Task<string> EncryptAsync(string plaintext, KeyOptions keyOptions, CancellationToken ct = default)
+        public Task<string> EncryptAsync(string plaintext, KeyOptions keyOptions, CancellationToken ct = default)
+        {
+            return keyOptions.KeyType switch
+            {
+                KeyType.RSA => EncryptAsync(plaintext, keyOptions, ASYMMETRIC_ALGORITHM, ct),
+                KeyType.Symmetric => EncryptAsync(plaintext, keyOptions, SYMMETRIC_ALGORITHM, ct),
+                KeyType.ECC => throw new NotImplementedException(),
+                _ => throw new ArgumentException($"{keyOptions.KeyType} is not supported by this provider"),
+            };
+        }
+
+        public Task<string> DecryptAsync(string ciphertext, KeyOptions keyOptions, CancellationToken ct = default)
+        {
+            return keyOptions.KeyType switch
+            {
+                KeyType.RSA => DecryptAsync(ciphertext, keyOptions, ASYMMETRIC_ALGORITHM, ct),
+                KeyType.Symmetric => DecryptAsync(ciphertext, keyOptions, SYMMETRIC_ALGORITHM, ct),
+                KeyType.ECC => throw new NotImplementedException(),
+                _ => throw new ArgumentException($"{keyOptions.KeyType} is not supported by this provider"),
+            };
+        }
+
+        private async Task<string> EncryptAsync(string plaintext, KeyOptions keyOptions, string algorithm, CancellationToken ct = default)
         {
             var keyHandle = await GetKeyHandle(keyOptions.KeyId);
 
             var payload = new
             {
                 keyHandle,
-                algorithm = "RSA-PKCS1",
+                algorithm,
                 plaintext
             };
 
-            using var request = new HttpRequestMessage(new HttpMethod("POST"), ENCRYPT_ENDPOINT);
-            request.Content = new StringContent(JsonConvert.SerializeObject(payload));
-            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
-
-            var response = await _httpClient.SendAsync(request, ct);
-            var json = await response.Content.ReadAsStringAsync(ct);
+            var json = await SendRequestAsync(payload, ENCRYPT_ENDPOINT, ct);
             var ciphertext = JObject.Parse(json)["ciphertext"].ToString();
 
             return ciphertext;
         }
 
-        public async Task<string> DecryptAsync(string ciphertext, KeyOptions keyOptions, CancellationToken ct = default)
+        private async Task<string> DecryptAsync(string ciphertext, KeyOptions keyOptions, string algorithm, CancellationToken ct = default)
         {
             var keyHandle = await GetKeyHandle(keyOptions.KeyId);
 
             var payload = new
             {
                 keyHandle,
-                algorithm = "RSA-PKCS1",
+                algorithm,
                 ciphertext
             };
 
-            using var request = new HttpRequestMessage(new HttpMethod("POST"), DECRYPT_ENDPOINT);
-            request.Content = new StringContent(JsonConvert.SerializeObject(payload));
-            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
-
-            var response = await _httpClient.SendAsync(request, ct);
-            var json = await response.Content.ReadAsStringAsync(ct);
+            var json = await SendRequestAsync(payload, DECRYPT_ENDPOINT, ct);
             var plaintext = JObject.Parse(json)["plaintext"].ToString();
 
             return plaintext;
@@ -67,6 +83,16 @@
         {
             // TODO Find a way to get the keyHandle for the keyId
             return null;
+        }
+
+        private async Task<string> SendRequestAsync(object payload, string endpoint, CancellationToken ct = default)
+        {
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), endpoint);
+            request.Content = new StringContent(JsonConvert.SerializeObject(payload));
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+            var response = await _httpClient.SendAsync(request, ct);
+            return await response.Content.ReadAsStringAsync(ct);
         }
     }
 }
