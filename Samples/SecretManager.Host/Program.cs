ddc62cb5c8af4858b586ae1e7caacf7a7157e933
Michiel van Schaik <mivansch@microsoft.com>
2021-11-11 12:22:19 +0100

    Resolved build errors on method renames.

--- Samples/SecretManager.Host/Program.cs
+++ Samples/SecretManager.Host/Program.cs
@@ -26,28 +26,28 @@
 
             string keyA = "test";
 
-            await manager.SetSecretAsync(keyA, "1234");
-            string valueA1 = await manager.GetSecretAsync(keyA);
+            await manager.SetSecretValueAsync(keyA, "1234");
+            string valueA1 = await manager.GetSecretValueAsync(keyA);
             Console.WriteLine($"Key '{keyA}' has value '{valueA1}'");
 
-            string valueA2 = await manager.GetSecretAsync(keyA);
+            string valueA2 = await manager.GetSecretValueAsync(keyA);
             Console.WriteLine($"Key '{keyA}' has value '{valueA2}'");
 
-            await manager.SetSecretAsync(keyA, "abcdef");
-            string valueA3 = await manager.GetSecretAsync(keyA);
+            await manager.SetSecretValueAsync(keyA, "abcdef");
+            string valueA3 = await manager.GetSecretValueAsync(keyA);
             Console.WriteLine($"Key '{keyA}' has value '{valueA3}'");
 
             string keyB = "secret";
 
-            await manager.SetSecretAsync(keyB, "azure");
-            string valueB1 = await manager.GetSecretAsync(keyB);
+            await manager.SetSecretValueAsync(keyB, "azure");
+            string valueB1 = await manager.GetSecretValueAsync(keyB);
             Console.WriteLine($"Key '{keyB}' has value '{valueB1}'");
 
-            string valueB2 = await manager.GetSecretAsync(keyB);
+            string valueB2 = await manager.GetSecretValueAsync(keyB);
             Console.WriteLine($"Key '{keyB}' has value '{valueB2}'");
 
-            await manager.SetSecretAsync(keyB, "veryverysecret");
-            string valueB3 = await manager.GetSecretAsync(keyB);
+            await manager.SetSecretValueAsync(keyB, "veryverysecret");
+            string valueB3 = await manager.GetSecretValueAsync(keyB);
             Console.WriteLine($"Key '{keyB}' has value '{valueB3}'");
         }
 
