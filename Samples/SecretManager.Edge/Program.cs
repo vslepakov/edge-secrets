8cacea5cce691acb7b173ea423fd3ebbdfec0648
Michiel van Schaik <mivansch@microsoft.com>
2021-12-07 11:24:58 +0100

    Added database connection and read to Edge Sample.

--- Samples/SecretManager.Edge/Program.cs
+++ Samples/SecretManager.Edge/Program.cs
@@ -6,6 +6,7 @@ namespace EdgeSecrets.Samples.SecretManager.Edge
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+    using Microsoft.Data.SqlClient;
     using EdgeSecrets.CryptoProvider;
     using EdgeSecrets.SecretManager;
 
@@ -15,8 +16,7 @@ namespace EdgeSecrets.Samples.SecretManager.Edge
         {
             Init().Wait();
 
-            // Get secrets
-            GetSecrets().Wait();
+            ReadDatabase().Wait();
 
             // Wait until the app unloads or is cancelled
             var cts = new CancellationTokenSource();
@@ -57,33 +57,37 @@ namespace EdgeSecrets.Samples.SecretManager.Edge
             Console.WriteLine("IoT Hub module client initialized.");
         }
 
-        static async Task GetSecrets()
+        static async Task ReadDatabase()
         {
+            Console.WriteLine("Get data from database.");
+
             (ICryptoProvider? cryptoProvider, string? keyId) = Configuration.GetCryptoProvider();
             Console.WriteLine($"Using Crypto Provider {cryptoProvider?.GetType()}");
 
             string secretsFile = "/usr/local/cache/secrets.json";
-
-            //// Get from file
-
-            // var manager = new SecretManagerClient()
-            //     .WithFileSecretStore(secretsFile, cryptoProvider, keyId)
-            //     .WithInMemoryStore();
-
-            //// Get from remote
-
-            var manager = new SecretManagerClient()
-                .WithRemoteSecretStore(TransportType.Amqp_Tcp_Only, new ClientOptions())
+            var secretManagerClient = new SecretManagerClient()
+                .WithRemoteSecretStore(TransportType.Mqtt_Tcp_Only, new ClientOptions())
                 .WithFileSecretStore(secretsFile, cryptoProvider, keyId)
                 .WithInMemorySecretStore();
-            
-            //// Test the secret store
+            Console.WriteLine("Secret manager client created.");
 
-            string keyA = "test";
-            var valueA1 = await manager.GetSecretValueAsync(keyA, null, DateTime.Now);
-            if (valueA1 != null) Console.WriteLine($"Key '{keyA}' has value '{valueA1}' (first read)");
-            var valueA2 = await manager.GetSecretValueAsync(keyA, null, DateTime.Now);
-            if (valueA2 != null) Console.WriteLine($"Key '{keyA}' has value '{valueA2}' (second read)");
+            string? dbConnectionString = await secretManagerClient.GetSecretValueAsync("FabrikamConnectionString", null, DateTime.Now);
+            if (!string.IsNullOrEmpty(dbConnectionString))
+            {
+                Console.WriteLine($"Valid database connection string found.");
+
+                using SqlConnection connection = new SqlConnection(dbConnectionString);
+                String sql = "SELECT ProductID, Name, ProductNumber, Color, Size, Weight FROM [SalesLT].[Product]";
+
+                using SqlCommand command = new SqlCommand(sql, connection);
+                connection.Open();
+
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetInt32(0)} {reader.GetString(1)} {reader.GetString(2)}");
+                }
+            }
         }
     }
 }
